b6fbb06e93f03274f25309e2e03c340511a63d37
lifpro <ysalissou@gmail.com>
2026-07-18 11:53:26 +0000

    service

---
+++ src/main/java/com/myshop/catalog_service/service/ProduitService.java
@@ -0,0 +1,22 @@
+package com.myshop.catalog_service.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProduitService {
+    public ProduitService() {
+
+    }
+    public void saveProduit() {
+    }
+    public void deleteProduit() {
+    }
+    public void updateProduit() {
+    }
+    public void getProduit() {
+    }
+    public void getAllProduit() {
+    }
+    public void getProduitByCategorie() {
+    }
+}
